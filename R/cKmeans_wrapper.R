6e94cb219afd5ecb7c35f76dcf9ead2f2ec3d945
ericreed <erreed@schoolph.umass.edu>
2020-03-11 15:14:48 -0400

    Added contrained Kmeans clustering

---
+++ R/cKmeans_wrapper.R
@@ -0,0 +1,39 @@
+#' Wrapper for hierarchical clustering
+#'
+#' This fuction is a wrapper for the constrained Kmeans algorithm using 
+#' `lcvqe` from the `conclust` package.
+#' 
+#' @param dataMatrix An P x N numeric matrix of data
+#' @param clustList List of objects to use for clustering procedure.
+#' @return A character string of concatenated 1's and 2's pertaining to the cluster assignment of each column in dataMatrix.
+#' @keywords clustering
+#' @export
+#' @examples
+#' cKmeans_wrapper(dataMatrix, clusList = list())
+#' 
+
+cKmeans_wrapper <- function(dataMatrix, clustList) {
+  
+  require(conclust)
+  eMatSub <- clustList$eMat[rownames(dataMatrix), clustList$labs %in% colnames(dataMatrix)]
+  labsSub <- clustList$labs[clustList$labs %in% colnames(dataMatrix)]
+  
+  # Get constraints
+  mustLink <- outer(labsSub, labsSub, "==")
+  mustLink[upper.tri(mustLink, diag = TRUE)] <- FALSE
+  mustLink <- which(mustLink, arr.ind = TRUE)
+  
+  # Cluster data
+  dClust = factor(lcvqe(t(eMatSub), 
+                        k = 2, 
+                        mustLink = mustLink, 
+                        cantLink = matrix(c(1, 1), 
+                                          nrow = 1), maxIter = clustList$maxIter), levels = c(1, 2))
+  
+  # Get label-level clusters
+  dMat <- as.matrix(table(dClust, labsSub))[,colnames(dataMatrix)]
+  modVec <- apply(dMat, 2, which.max)
+  mods <- paste(modVec, collapse = "")
+  
+  return(mods)
+}
--- R/hclust_wrapper.R
+++ R/hclust_wrapper.R
@@ -2,7 +2,7 @@
 #'
 #' This fuction is a wrapper for the hclust, dist, and cutree functions. It outputs a string of a concatenated vector of 1's and 2's indexed by column.
 #' @param dataMatrix An P x N numeric matrix of data
-#' @param clustList List of objects to use for clustering procedure (Note used in this function).
+#' @param clustList List of objects to use for clustering procedure.
 #' @return A character string of concatenated 1's and 2's pertaining to the cluster assignment of each column in dataMatrix.
 #' @keywords clustering
 #' @export
